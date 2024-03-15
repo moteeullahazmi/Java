dfee931fe18e46cb3693f45b7ac31b31e01c2b06
Moteeullah Azmi <you@example.com>
2024-03-15 17:14:53 +0530

    Java

--- 01JAVA/Java Foundation Extra/src/While.java
+++ 01JAVA/Java Foundation Extra/src/While.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class While {
+public class While {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
 }
