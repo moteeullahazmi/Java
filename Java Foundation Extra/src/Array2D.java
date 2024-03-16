2d02289fd10c69fac1aa5ba3583f9f24b46e512c
Moteeullah Azmi <you@example.com>
2024-03-16 14:37:56 +0530

    azmi

--- Java Foundation Extra/src/Array2D.java
+++ Java Foundation Extra/src/Array2D.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Array2D {
+import java.util.Scanner;
+public class Array2D {
+    public static void main(String[] args) {
+//        int[][] marks = new arr[3][3];
+        Scanner in= new Scanner(System.in);
+
+        for(int i=0; i<=marks.length-1;i++){
+            for (int j=0; j<marks[i].length;j++){
+                System.out.println(+i+ "Enter a " +j+ "the value is");
+                int data=in.nextInt();
+            }
+        }
+
+
+
+
+
+    }
 }
--- Java Foundation Extra/src/Arrays.java
+++ Java Foundation Extra/src/Arrays.java
@@ -2,7 +2,7 @@ public class Arrays {
     public static void main(String[] args) {
         String[] names= {"abc","def","ghi","ijk"};
         names[1]="Moteeullah Azmi";
-        for(int i=0; i<= names.length-1;i++){
+        for(int i=0; i< names.length;i++){
             System.out.println(names[i]);
         }
     }
