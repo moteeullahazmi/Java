05fdeafbe754a2a444882f0bc4d862d54f629c47
Moteeullah Azmi <you@example.com>
2024-03-16 15:33:40 +0530

    azmi

--- Java Foundation Extra/src/ArraysD.java
+++ Java Foundation Extra/src/ArraysD.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 public class ArraysD {
     public static void main(String[] args) {
         int[][] marks = new int[3][3];
-        Scanner in= new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        for(int i=0; i<=marks.length-1;i++){
-            for (int j=0; j<marks[i].length;j++){
-                System.out.println(+i+ "Enter a " +j+ "the value is");
-                int data=in.nextInt();
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j <= marks[i].length - 1; j++) {
+                System.out.println("enter " + i + " Enter a " + j + " the value is");
+                marks[i][j] = in.nextInt();
             }
         }
-
-
-
-
-
+        System.out.println("the element are: ");
+        for (int i = 0; i <= marks.length - 1; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                System.out.print(marks[i][j] );
+            }
+            System.out.println(",");
+        }
     }
 }
+
+
