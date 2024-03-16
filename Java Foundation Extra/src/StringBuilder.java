bf6457119a5d09210fe1b8cb0fd345a24e51c574
Moteeullah Azmi <you@example.com>
2024-03-16 16:41:38 +0530

    az

--- Java Foundation Extra/src/StringBuilder.java
+++ Java Foundation Extra/src/StringBuilder.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class StringBuilder {
+public class StringBuilder {
+    public static void main(String[] args) {
+        String str= "Moteeullah ";
+        str.concat("Azmi");
+        System.out.println(str); //Moteeullah because immutable
+
+        StringBuffer sb = new StringBuffer("Moteeullah ");
+        sb.append("Azmi");
+        System.out.println(sb);
+    }
+
 }
