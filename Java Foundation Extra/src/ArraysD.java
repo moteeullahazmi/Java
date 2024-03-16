e3827393f32dad1b9990824e298a849f235c531a
Moteeullah Azmi <you@example.com>
2024-03-16 14:43:13 +0530

    azmi

--- Java Foundation Extra/src/ArraysD.java
+++ Java Foundation Extra/src/ArraysD.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ArraysD {
+import java.util.Scanner;
+public class ArraysD {
+    public static void main(String[] args) {
+        int[][] marks = new int[3][3];
+        Scanner in= new Scanner(System.in);
+
+        for(int i=0; i<=marks.length-1;i++){
+            for (int j=0; j<marks[i].length;j++){
+                System.out.println(+i+ "Enter a " +j+ "the value is");
+                int data=in.nextInt();
+            }
+        }
+
+
+
+
+
+    }
 }
