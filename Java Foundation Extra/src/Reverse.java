dae55da077b876dd72b19637ca7d285d35e5d0ce
Moteeullah Azmi <you@example.com>
2024-03-17 16:42:33 +0530

    all java

--- Java Foundation Extra/src/Reverse.java
+++ Java Foundation Extra/src/Reverse.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Reverse {
+import java.util.Scanner;
+public class Reverse {
+    public static void main(String[] args) {
+        System.out.print("Enter reverse string:");
+        Scanner in = new Scanner(System.in);
+        String name= in.nextLine();
+        String reverse ="";
+
+        for(int i=name.length()-1; i>=0;i--){
+            reverse= reverse+ name.charAt(i);
+
+        }
+    System.out.println(reverse);
+    }
 }
